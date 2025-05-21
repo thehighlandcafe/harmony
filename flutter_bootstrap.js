0918c79b7fd8b60b0217d614340feddee455a820
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2025-05-21 19:24:05 +0100

    Updated to v3.1.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3242959297"
+    serviceWorkerVersion: "3556415082"
   }
 });
